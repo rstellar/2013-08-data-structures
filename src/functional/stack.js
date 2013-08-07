ec4ebf496403a6aaad57bb06c9771a1d42fba564
Hack Reactor Students <github@hackreactor.com>
2013-08-07 14:21:06 -0700

    reports size correctly, passes test

--- src/functional/stack.js
+++ src/functional/stack.js
@@ -2,18 +2,23 @@ var makeStack = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
   var stack = {};
 
   stack.push = function(value){
+    size++;
+
   };
 
   stack.pop = function(){
+    size--;
+    if (size <0) size = 0;
   };
 
   stack.size = function(){
+  return size;
   };
 
   return stack;
