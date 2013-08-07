0df4f8bb742eee200ef27784c46a4883914b3bb0
Hack Reactor Students <github@hackreactor.com>
2013-08-07 16:07:37 -0700

    moved to functional-shared style, implemented push and pop in new style.

--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -1,3 +1,51 @@
+
+
 var makeStack = function(){
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  //should we keep storage and size in this outer scope?
+  //or do they need to be a part of each new Stack?
+  var newStack = {
+    "_storage": {},
+    "_size": 0
+  };
+
+  _.extend(newStack, makeStack.stackMethods);
+
+
+  //move all this stuff
+  /*
+  stack.pop = 
+  };
+
+  stack.size = function(){
+    return size;
+  };
+  */
+
+  return newStack;
 };
+
+
+
+//create methods within makeStack
+makeStack.stackMethods = {};
+
+makeStack.stackMethods.whatever = function(){};
+
+makeStack.stackMethods.push = function(value){
+    this._size++;
+    this._storage[this._size] = value;
+};
+
+makeStack.stackMethods.pop = function(){
+    var temp = this._storage[this._size];
+    delete this._storage[this._size];
+
+    this._size--;
+    if (this._size <0) this._size = 0;
+    return temp;
+};
+
+// create methods for pop, push size?
+//something like makeStack.makeMethods.pop (push,size, etc)
+// makeStack.makeMethods.pop = function(){
+//
