160bf785ef21b082a295c9bdff190d84ea713346
Hack Reactor Students <github@hackreactor.com>
2013-08-07 15:10:46 -0700

    initial state on new branch using array

--- src/functional/queue.js
+++ src/functional/queue.js
@@ -3,21 +3,34 @@ var makeQueue = function(){
   var storage = {};
 
   var size = 0; // Hint: set an initial value here
-
+  var first = 1;
   // Implement the methods below
   var queue = {};
 
   queue.enqueue = function(value){
     size++;
+    storage[size] = value;
   };
 
   queue.dequeue = function(){
     size--;
     if (size < 0) size = 0;
+    var temp = storage[first];
+
+    delete storage[first];
+    first++;
+    return temp;
   };
 
   queue.size = function(){
     return size;
+  };
+
+  queue.viewself = function(){
+    return {storage : storage,
+      first : first};
+
+
   };
 
   return queue;
