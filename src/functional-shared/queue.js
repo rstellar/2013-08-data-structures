70101c08f5a6b0bbc9fb8cec2d1e3d200dabaf89
Hack Reactor Students <github@hackreactor.com>
2013-08-07 17:01:47 -0700

    reimplemented all queue methods in function-shared style

--- src/functional-shared/queue.js
+++ src/functional-shared/queue.js
@@ -1,3 +1,23 @@
 var makeQueue = function(){
-	// Hey! Copy your code from src/functional/queue.js and paste it here
+  //var storage = [];
+  // Implement the methods below
+  var newQueue = {_storage: []};
+
+  _.extend(newQueue, makeQueue.queueMethods);
+  return newQueue;
 };
+
+
+makeQueue.queueMethods = {};
+makeQueue.queueMethods.enqueue = function(value){
+    this._storage.push(value);
+};
+
+makeQueue.queueMethods.dequeue = function(){
+  return this._storage.shift();
+};
+
+makeQueue.queueMethods.size = function(){
+  return this._storage.length;
+};
+
