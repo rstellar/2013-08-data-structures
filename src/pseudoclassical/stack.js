e16903f6ff6c7e4a412ef4d314a66767931d88d0
Hack Reactor Students <github@hackreactor.com>
2013-08-07 19:49:14 -0700

    convert Stack from prototypal style to pseudo-classical style, passes all tests

--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,3 +1,22 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this._storage = {};
+  this._size = 0;
+};
+
+Stack.prototype.push = function(value){
+    this._size++;
+    this._storage[this._size] = value;
+};
+
+Stack.prototype.pop = function(){
+    var temp = this._storage[this._size];
+    delete this._storage[this._size];
+
+    this._size--;
+    if (this._size <0) this._size = 0;
+    return temp;
+};
+
+Stack.prototype.size = function(){
+  return this._size;
 };
