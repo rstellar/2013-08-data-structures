e24c962e68f30831d1d330cb8788fb98bf532ad0
Hack Reactor Students <github@hackreactor.com>
2013-08-07 17:23:33 -0700

    translate the functional-shared style queue to prototypal inheritance

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,3 +1,20 @@
 var Queue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  var newQueue = Object.create(Queue.blueprint);
+  newQueue._storage = [];
+  return newQueue;
+};
+
+
+Queue.blueprint = {};
+
+Queue.blueprint.enqueue = function(value){
+    this._storage.push(value);
+};
+
+Queue.blueprint.dequeue = function(){
+  return this._storage.shift();
+};
+
+Queue.blueprint.size = function(){
+  return this._storage.length;
 };
