a61f27b5767fad5d09fe30f15e8eba641e97a6d6
Hack Reactor Students <github@hackreactor.com>
2013-08-07 17:12:20 -0700

    translate functional-shared into prototypal style

--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,3 +1,31 @@
+//why attach the prototype to the actual Stack function (i.e. the constructor?)?
+//
+
 var Stack = function() {
   // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var newStack = Object.create(Stack.blueprint);
+  newStack._storage = {};
+  newStack._size = 0;
+  
+  return newStack;
+};
+
+Stack.blueprint = {};
+
+Stack.blueprint.push = function(value){
+    this._size++;
+    this._storage[this._size] = value;
+};
+
+Stack.blueprint.pop = function(){
+    var temp = this._storage[this._size];
+    delete this._storage[this._size];
+
+    this._size--;
+    if (this._size <0) this._size = 0;
+    return temp;
+};
+
+Stack.blueprint.size = function(){
+  return this._size;
 };
